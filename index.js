dee585c9c1b6b46452c80cc55059c308948e2c28
Lewis1Ndungu <lewis.ndungu@student.moringaschool.com>
2022-11-26 16:30:04 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  //this function should take in a employee Object, a key and a value. The function should not mutate the employee parameter and return a new employee that has an updated value for the key passed in.
+  return Object.assign({}, employee, { [key]: value });
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  //this function should work the same as updateEmployeeWithKeyAndValue() but it should mutate the employee parameter passed in.
+  employee[key] = value;
+
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  // this function should take in a employee Object and a key. It should delete the key/value pair for the key that was passed in from the employee Object. This should all not actually mutate the employee passed in.
+  const NEW_OBJ =  Object.assign({}, employee);
+  delete NEW_OBJ[key];
+  return NEW_OBJ;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//this function should work the same as deleteFromEmployeeByKey() but it should mutate the employee passed in.
+Object.assign({}, employee);
+delete employee[key];
+return employee;
+}
